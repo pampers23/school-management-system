53440a8e29f10d74abc9396e6dbadbc6a98c1d73
pampers23 <dexterpimss23@gmail.com>
2026-07-21 08:35:59 +0800

    feat(teachers): implement teacher CRUD and account deactivation

--- src/teachers/teachers.controller.ts
+++ src/teachers/teachers.controller.ts
@@ -1,4 +1,58 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  UseGuards,
+  Get,
+  Patch,
+  Delete,
+  Param,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { TeachersService } from './teachers.service';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { Role } from '../../generated/prisma/enums';
+import { CreateTeacherDto } from './dto/create-teacher.dto';
+import { UpdateTeacherDto } from './dto/update-teacher.dto';
 
 @Controller('teachers')
-export class TeachersController {}
+export class TeachersController {
+  constructor(private readonly teacherService: TeachersService) {}
+
+  @Post('add-teacher')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.ADMIN)
+  create(@Body() dto: CreateTeacherDto) {
+    return this.teacherService.create(dto);
+  }
+
+  @Get()
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.ADMIN)
+  findAll() {
+    return this.teacherService.findAll();
+  }
+
+  @Get(':id')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.ADMIN)
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.teacherService.findOne(id);
+  }
+
+  @Patch(':id')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.ADMIN)
+  update(@Param('id', ParseIntPipe) id: number, @Body() dto: UpdateTeacherDto) {
+    return this.teacherService.update(id, dto);
+  }
+
+  @Delete(':id')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.ADMIN)
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.teacherService.remove(id);
+  }
+}
--- src/teachers/teachers.service.ts
+++ src/teachers/teachers.service.ts
@@ -1,4 +1,125 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { CreateTeacherDto } from './dto/create-teacher.dto';
+import { PrismaService } from '../prisma/prisma.service';
+import { Role } from '../../generated/prisma/enums';
+import * as bcrypt from 'bcrypt';
+import { UpdateTeacherDto } from './dto/update-teacher.dto';
 
 @Injectable()
-export class TeachersService {}
+export class TeachersService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(dto: CreateTeacherDto) {
+    const existingTeacher = await this.prisma.teacher.findUnique({
+      where: {
+        employeeId: dto.employeeId,
+      },
+    });
+
+    if (existingTeacher) {
+      throw new BadRequestException('Employee ID is already exists');
+    }
+
+    const temporaryPassword = 'Temp@123';
+
+    const hashedPassword = await bcrypt.hash(temporaryPassword, 10);
+
+    return this.prisma.$transaction(async (tx) => {
+      const user = await tx.user.create({
+        data: {
+          username: dto.employeeId,
+          password: hashedPassword,
+          role: Role.TEACHER,
+          mustChangePassword: true,
+        },
+      });
+
+      const teacher = await tx.teacher.create({
+        data: {
+          employeeId: dto.employeeId,
+          firstName: dto.firstName,
+          lastName: dto.lastName,
+          middleName: dto.middleName,
+          extensionName: dto.extensionName,
+          userId: user.id,
+        },
+      });
+
+      return {
+        message: 'Teacher created Successfully',
+        username: user.username,
+        temporaryPassword,
+        teacher,
+      };
+    });
+  }
+
+  async findAll() {
+    return this.prisma.teacher.findMany({
+      where: {
+        user: {
+          isActive: true,
+        },
+      },
+      include: {
+        user: {
+          select: {
+            username: true,
+            isActive: true,
+          },
+        },
+      },
+    });
+  }
+
+  async findOne(id: number) {
+    const teacher = await this.prisma.teacher.findUnique({
+      where: { id },
+      include: {
+        user: {
+          select: {
+            username: true,
+            role: true,
+            isActive: true,
+          },
+        },
+      },
+    });
+
+    if (!teacher) {
+      throw new NotFoundException('Teacher not found');
+    }
+
+    return teacher;
+  }
+
+  async update(id: number, dto: UpdateTeacherDto) {
+    await this.findOne(id);
+
+    return this.prisma.teacher.update({
+      where: {
+        id,
+      },
+      data: dto,
+    });
+  }
+
+  async remove(id: number) {
+    const teacher = await this.findOne(id);
+
+    await this.prisma.user.update({
+      where: {
+        id: teacher.userId,
+      },
+      data: {
+        isActive: false,
+      },
+    });
+
+    return { message: 'Teacher deactivated successfully' };
+  }
+}
